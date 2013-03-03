89e1e48f48bf7083ec8692b86f84ce0b0b10b510
Zachary Tong <zacharyjtong@gmail.com>
2013-03-03 07:13:37 -0500

    Change hardcoded localhost:9200 to window.location.host
    Localhost had been hardcoded for testing, but was not removed when pushed.
    This fix removes localhost and defaults to window.location.host, which
    is basically whatever has been entered in the URL.

--- _site/js/controllers/NavbarCtrl.js
+++ _site/js/controllers/NavbarCtrl.js
@@ -2,6 +2,8 @@
 function NavbarCtrl($scope, $rootScope, $route, $location, Data) {
     $scope.data = Data;
 
+    $scope.data.host = 'http://' + window.location.host;
+
     $scope.isActive = function(route) {
 
         var path = $location.path();
