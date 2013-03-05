91a02069d50f5f9a9c93d2f4dc33d2397b64594d
Zachary Tong <zacharyjtong@gmail.com>
2013-03-05 09:59:33 -0500

    Refactor js loader to load as many scripts in parallel as possible

--- _site/js/load.js
+++ _site/js/load.js
@@ -1,38 +1,25 @@
 (function() {
     "use strict";
-    head.js({
-        json2: "js/vendor/json2.js"
-    }, {
-        jsonlint: "js/vendor/jsonlint.js"
-    }, {
-        angularjs: "js/vendor/angular.min.js"
-    }, {
-        angularsanitize: "js/vendor/angular-sanitize.min.js"
-    }, {
-        ngbootstrap: "js/vendor/ng-bootstrap/ui-bootstrap-custom-tpls-0.1.0-SNAPSHOT.min.js"
-    }, {
-        app: "js/app.js"
-    }, {
-        dropdownctrl: "js/controllers/DropdownCtrl.js"
-    }, {
-        analyzerctrl: "js/controllers/AnalyzerCtrl.js"
-    }, {
-        analyzerctrl: "js/controllers/TokenizerCtrl.js"
-    }, {
-        navbarctrl: "js/controllers/NavbarCtrl.js"
-    }, {
-        QueryInput: "js/controllers/QueryInput.js"
-    }, {
-        QueryOutput: "js/controllers/QueryOutput.js"
-    }, {
-        modal: "js/directives/modal.js"
-    }, {
-        rawerror: "js/directives/rawerror.js"
-    }, {
-        jsonlintfilter: "js/filters/jsonlint.js"
-    },{
-        lucenequeryparser: "js/vendor/lucene/lucene-query-parser.js"
-    } );
+
+    head.js("js/vendor/json2.js")
+        .js("js/vendor/jsonlint.js")
+        .js("js/vendor/lucene/lucene-query-parser.js")
+        .js("js/vendor/angular.min.js", function() {
+            head.js("js/vendor/angular-sanitize.min.js","js/vendor/ng-bootstrap/ui-bootstrap-custom-tpls-0.1.0-SNAPSHOT.min.js", "js/app.js", function() {
+                head.js("js/controllers/DropdownCtrl.js")
+                    .js("js/controllers/AnalyzerCtrl.js")
+                    .js("js/controllers/TokenizerCtrl.js")
+                    .js("js/controllers/NavbarCtrl.js")
+                    .js("js/controllers/QueryInput.js")
+                    .js("js/controllers/QueryOutput.js")
+                    .js("js/directives/modal.js")
+                    .js("js/directives/rawerror.js")
+                    .js("js/filters/jsonlint.js");
+            });
+        });
+
+
+
 
 }).call(this);
 
