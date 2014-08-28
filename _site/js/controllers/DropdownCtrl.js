1d446df7ff3b408639aaf72811f394bad83394f3
tintin04 <zuken21@yahoo.com>
2014-08-28 11:01:22 +0700

    Update DropdownCtrl.js
    Fix mistyping indices and response data to work with 0.90.x

--- _site/js/controllers/DropdownCtrl.js
+++ _site/js/controllers/DropdownCtrl.js
@@ -14,7 +14,7 @@ function DropdownCtrl($scope, $http, Data, pubsub) {
       var path = $scope.data.host + "/_mapping";
       $http.get(path).then(function(response){
           if($scope.indices && $scope.indices.length > 0){
-              $scope.indicies.length = 0;
+              $scope.indices.length = 0;
           }
           $scope.data.mapping = response.data;
 
@@ -22,7 +22,7 @@ function DropdownCtrl($scope, $http, Data, pubsub) {
 
               $scope.indices.push(i);
               $scope.types[i] = [];
-              for (j in response.data[i].mappings){
+              for (j in response.data[i]){
 
                   $scope.types[i].push(j);
               }
