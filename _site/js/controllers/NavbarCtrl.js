4124f1e4fb53435e03902bbc1c5232da9e94582a
Christophe Coevoet <stof@notk.org>
2017-12-20 10:41:52 +0100

    Improve the default host value
    - add support for passing the ES host in a query string parameter, allowing
      for a better experience for standalone deployments
    - use the current location only when the site runs as an ES site plugin

--- _site/js/controllers/NavbarCtrl.js
+++ _site/js/controllers/NavbarCtrl.js
@@ -2,8 +2,6 @@
 function NavbarCtrl($scope, $rootScope, $route, $location, Data) {
     $scope.data = Data;
 
-    $scope.data.host = window.location.protocol + '//' + window.location.host;
-
     $scope.isActive = function(route) {
 
         var path = $location.path();
